57a45762668690a8e45f129acbfbb02ae139e90b
KazanDzibre <markoruzic997@gmail.com>
2020-01-18 01:24:23 +0100

    dodao ovaj kurac za figlet

--- config.py
+++ config.py
@@ -23,6 +23,13 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+#
+#  __  __            _           ____            _
+# |  \/  | __ _ _ __| | _____   |  _ \ _   _ ___(_) ___
+# | |\/| |/ _` | '__| |/ / _ \  | |_) | | | |_  / |/ __|
+# | |  | | (_| | |  |   < (_) | |  _ <| |_| |/ /| | (__
+# |_|  |_|\__,_|_|  |_|\_\___/  |_| \_\\__,_/___|_|\___|
+
 
 import os
 import re
@@ -91,6 +98,7 @@ def init_keys():
         Key([mod], "Return", lazy.spawn(myTerm)),
         Key([mod], "s", lazy.spawn("stacer")),
         Key([mod],"w", lazy.spawn(browser)),
+        Key([mod],"x", lazy.spawn("scrot /home/rule/screenshots/%Y-%m-%d-%T-screenshot.png")),
         Key([mod],"q", lazy.spawn("qutebrowser")),
         Key([mod],"g", lazy.spawn("gimp")),
         Key([mod],"t", lazy.spawn("qbittorrent")),
